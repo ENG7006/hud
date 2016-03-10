fc62654679eb98f593254afa3682d0fcfacc1af8
tbreijak <ab6131@wayne.edu>
2016-03-09 19:59:04 -0500

    moved ball over
    moved ball to 250X

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var timer = new Timer();
 
 var ball = {
-  x: 80,
+  x: 250,
   y: 200,
   size: 15,
   speed: 2,
@@ -28,11 +28,11 @@ var ball = {
   bounce: function () {
     this.speed = this.speed * -1; // your code goes here; a hint: this will require making your own variable as well as defining this function
     // another hint: notice that ball is defined using object literal notation
+    // score one
+    counter.scoreOne();
+    // or: counter.number++; don't do this because it's a bad idea to manipulate somebody else's properties
   }
 };
-
-setup = function () {
-createCanvas(600, 400);//var minute = minute();
 var counter = {
   number: 0,
 
@@ -40,17 +40,28 @@ var counter = {
     textSize(15);
     fill("white");
     text(this.number, 60, 60);
-  
+    //  if (this.y > height - this.size / 2) this.bounce()counter+1;
   },
 
   update: function () {
-    if (ball.checkForBounce) this.number += 1;
-  }
+    //if (ball.checkForBounce()) this.number += 1;
+  },
+
+  scoreOne: function() {
+    this.number++;
+    // is the same as this.number = this.number + 1;
+    // is the same as this.number += 1;
+  },
+};
+
+setup = function () {
+createCanvas(600, 400);//var minute = minute();
+
 
 };
 
 
-};
+
 //  setup = function() {
      // setup is called only when the sketch starts
 //};
@@ -60,7 +71,7 @@ var counter = {
     ball.display();
     text(timer.getPrettyElapsedTime(), 20, 20);
     timer.getPrettyElapsedTime();
-    //counter.update();
-    //counter.display();
+    counter.update();
+    counter.display();
 
 };
